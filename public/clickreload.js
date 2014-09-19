fe28fdcfedbb1c0414ed1d9bd211264a0022e3a8
Ryan Yandle <ryandle@gmail.com>
2014-09-19 11:16:18 -0700

    update the document.onclick handler so that clicks to the "Fork me on github" image actually navigate to github instead of reload the current page.

--- public/clickreload.js
+++ public/clickreload.js
@@ -1,3 +1,8 @@
 document.onclick = function(e) {
+  // ignore clicks on the github fork image so that the page will navigate to github
+  if(e.target.id == 'gitHubForkImage') {
+    return;
+  }
+
   location.reload();
 }
